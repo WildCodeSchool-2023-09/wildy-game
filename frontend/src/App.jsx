32ba96743ee0e8deb73452c154e79a5d5b01ca92
Océane Lahoual <138216585+Midunighto@users.noreply.github.com>
2023-12-13 21:37:49 +0100

    background ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./styles/root.scss";
+import ParallaxCoin from "./components/Parallax";
 
 function App() {
   return (
     <div className="main">
       <Navbar />
+      <ParallaxCoin />
       <Outlet />
       <Footer />
     </div>

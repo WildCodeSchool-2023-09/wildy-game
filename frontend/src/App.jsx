d29070a39e545d797ada66268724ed7e2c623f1c
Océane Lahoual <138216585+Midunighto@users.noreply.github.com>
2023-12-13 11:28:27 +0100

    packages & variables init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,5 @@
-import React from "react";
-import { Parallax } from "react-parallax";
-import background from "./assets/images/FondGlobal.png";
 import "./styles/root.scss";
 
-function App() {
-  return (
-    <div className="main flex">
-      <Parallax bgImage={background} strength={1000}>
-        <h1 className="text-red-500">Wildy Gamy</h1>
-      </Parallax>
-    </div>
-  );
-}
+function App() {}
 
 export default App;

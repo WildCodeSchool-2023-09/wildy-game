dfc16ec64a887b097ab0073d7fc97c6656cfa169
ghispell <ghis.pell@hotmail.fr>
2023-12-15 10:58:30 +0100

    error page

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,9 +1,30 @@
+import { Link } from "react-router-dom";
+import borne from "../assets/images/borne_arcade_404.png";
+import ParallaxCoin from "../components/ParallaxCoin";
+import start from "../assets/video/wildy_gamy_gameover_carre.mp4";
+import "../styles/error.scss";
+
 function ErrorPage() {
   return (
-    <>
-      <p>Futur Page Erreur</p>
-      <p>Futur Page Erreur</p>
-    </>
+    <Link to="/">
+      <div className="wrapper-home">
+        <ParallaxCoin />
+
+        <div className="container-home">
+          <img src={borne} alt="borne 404" className="borne-arcade" />
+          <video
+            autoPlay
+            loop
+            controls={false}
+            playsInline
+            muted
+            className="start-video"
+          >
+            <source src={start} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </Link>
   );
 }
 

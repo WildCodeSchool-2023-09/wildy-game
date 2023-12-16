4a66350dcf3603d4351a3eeec7e2708f5f8d910c
Baris G <barisg@pm.me>
2023-12-16 21:02:01 +0100

    filtre des raretés fonctionnels

--- frontend/src/components/Boutique/InputCheckbox.jsx
+++ frontend/src/components/Boutique/InputCheckbox.jsx
@@ -1,13 +1,23 @@
 import PropTypes from "prop-types";
 
-function InputCheckbox({ value, setter, text }) {
+function InputCheckbox({ value, setArrayFilter, arrayFilter, text, reset }) {
+  const handleFilter = (prev) => {
+    if (!prev.includes(text)) {
+      setArrayFilter([...prev, text]);
+    } else {
+      setArrayFilter(prev.filter((element) => element !== text));
+    }
+  };
   return (
     <div className="flex">
       <input
         type="checkbox"
         checked={value}
         className="w-6 bg-color-first bouton-boutique"
-        onChange={() => setter((prev) => !prev)}
+        onChange={() => {
+          handleFilter(arrayFilter);
+          reset((prev) => !prev);
+        }}
       />
       <p className="pl-2">{text}</p>
     </div>
@@ -16,8 +26,10 @@ function InputCheckbox({ value, setter, text }) {
 
 InputCheckbox.propTypes = {
   value: PropTypes.bool.isRequired,
-  setter: PropTypes.func.isRequired,
+  setArrayFilter: PropTypes.func.isRequired,
   text: PropTypes.string.isRequired,
+  arrayFilter: PropTypes.arrayOf(PropTypes.string).isRequired,
+  reset: PropTypes.func.isRequired,
 };
 
 export default InputCheckbox;

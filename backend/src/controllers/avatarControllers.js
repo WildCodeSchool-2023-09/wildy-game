e28adb5d361e2b9d957349f031869cc6a2b53fe9
Ijuaa <pierre.colpart@gmail.com>
2023-12-16 02:20:03 +0100

    finalisation de tout le routing, manager et controller de avatar et ait enfin satisfait Eslint pour pouvoir commit

--- backend/src/controllers/avatarControllers.js
+++ backend/src/controllers/avatarControllers.js
@@ -36,18 +36,49 @@ const findById = async (req, res, next) => {
 
 // The E of BREAD - Edit (Update) operation
 // This operation is not yet implemented
+const edit = async (req, res, next) => {
+  // ??? erreur Eslint inconnue : Expected to return a value at the end of async arrow function
+  const id = parseInt(req.params.id, 10); // Si quelqu'un peut m'expliquer ce qu'est un *** de radix ! J'ai rien compris mais sur internet
+  // ils disaient de mettre 10 pour un nombre décimal.. et surtout pourquoi soudainement c'est important.
+  const { name, image, rarity } = req.body;
+
+  try {
+    const avatarToUpdate = {
+      id,
+      name,
+      image,
+      rarity,
+    };
+    const result = await tables.avatar.update(avatarToUpdate);
+    if (result.affectedRows === 0) {
+      return res.status(404).send("Aucun avatar trouvé avec cet ID.");
+    }
+
+    return res.status(200).send(`L'avatar ayant l'id: ${id} a été mis à jour.`);
+  } catch (err) {
+    next(err);
+    return res
+      .status(500)
+      .send(
+        "Une erreur s'est produite"
+      ); /* Il a fallu que j'ajoute un return ici
+    pour résoudre le soucis Eslint qui se déclarait à l'ouverture de la fonction edit (voir plus haut) */
+  }
+};
 
 // The A of BREAD - Add (Create) operation
 const add = async (req, res, next) => {
   // Extract the item data from the request body
-  const item = req.body;
+  const avatar = req.body;
 
   try {
     // Insert the item into the database
-    const insertId = await tables.item.create(item);
+    const insertId = await tables.avatar.create(avatar);
 
     // Respond with HTTP 201 (Created) and the ID of the newly inserted item
-    res.status(201).json({ insertId });
+    res.status(201).json({ insertId }); // <--- {insertID} est une syntaxe raccourcie! C'est possible en JS
+    // quand la clé et la valeur sont identiques.
+    // je pourrais écrire à la place {insertId : insertId}. Enfin si ESlint m'autorisait.
   } catch (err) {
     // Pass any errors to the error-handling middleware
     next(err);
@@ -56,12 +87,25 @@ const add = async (req, res, next) => {
 
 // The D of BREAD - Destroy (Delete) operation
 // This operation is not yet implemented
+const destroy = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await tables.avatar.delete(id);
 
+    if (result.affectedRows === 0) {
+      res.status(404).send("id pas trouvée");
+    } else {
+      res.status(200).send(`L'avatar ayant l'id: ${id} a bien été supprimée`);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 // Ready to export the controller functions
 module.exports = {
   browse,
   findById,
-  // edit,
   add,
-  // destroy,
+  edit,
+  destroy,
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -6,20 +6,15 @@ const router = express.Router();
 // Define Your API Routes Here
 /* ************************************************************************* */
 
-// Import itemControllers module for handling item-related operations
-const itemControllers = require("./controllers/itemControllers");
-// Route to get a list of items
-router.get("/items", itemControllers.browse);
-
-// Route to get a specific item by ID
-router.get("/items/:id", itemControllers.read);
-
-// Route to add a new item
-router.post("/items", itemControllers.add);
-
-/* ************************************************************************* */
 const avatarControllers = require("./controllers/avatarControllers");
 
 router.get("/avatars", avatarControllers.browse);
 router.get("/avatars/:id", avatarControllers.findById);
+
+router.post("/avatars", avatarControllers.add);
+
+router.put("/avatars/:id", avatarControllers.edit);
+
+router.delete("/avatars/:id", avatarControllers.destroy);
+
 module.exports = router;

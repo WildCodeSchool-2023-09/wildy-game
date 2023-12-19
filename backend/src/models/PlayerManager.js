c6485d74e5d719c2d6c080e82be76a5e3b0b34c9
Ijuaa <pierre.colpart@gmail.com>
2023-12-19 15:59:33 +0100

    update schema.sql + routing Player

---
+++ backend/src/models/PlayerManager.js
@@ -0,0 +1,99 @@
+const AbstractManager = require("./AbstractManager");
+
+class PlayerManager extends AbstractManager {
+  constructor() {
+    // Call the constructor of the parent class (AbstractManager)
+    // and pass the table name "item" as configuration
+    super({ table: "player" });
+  }
+
+  // The C of CRUD - Create operation
+
+  async create(player) {
+    // Execute the SQL INSERT query to add a new item to the "item" table
+    const [result] = await this.database.query(
+      `insert into ${this.table} (id, firstname, lastname, pseudo, password, email, experience, credit, membreId, profilTheme, lvl) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+      [
+        player.id,
+        player.firstname,
+        player.lastname,
+        player.pseudo,
+        player.password,
+        player.email,
+        player.experience,
+        player.credit,
+        player.membreId,
+        player.profilTheme,
+        player.lvl,
+      ]
+    );
+
+    // Return the ID of the newly inserted item
+    return result.insertId;
+  }
+
+  // The Rs of CRUD - Read operations
+
+  async read(id) {
+    // Execute the SQL SELECT query to retrieve a specific item by its ID
+    const [rows] = await this.database.query(
+      `select * from ${this.table} where id = ?`,
+      [id]
+    );
+
+    // Return the first row of the result, which represents the item
+    return rows[0];
+  }
+
+  async readAll() {
+    // Execute the SQL SELECT query to retrieve all items from the "item" table
+    const [rows] = await this.database.query(`select * from ${this.table}`);
+
+    // Return the array of items
+    return rows;
+  }
+
+  // The U of CRUD - Update operation
+  // TODO: Implement the update operation to modify an existing item
+
+  async update(player) {
+    if (!player.id) {
+      throw new Error("Un ID est nécessaire pour mettre à jour l'avatar.");
+    }
+
+    const query = `UPDATE ${this.table} SET firstname = ?, lastname = ?, pseudo = ?, password= ?, email= ?, experience= ?, credit= ?, membreId= ?, profilTheme= ?, lvl= ? WHERE id = ?`;
+
+    const [result] = await this.database.query(query, [
+      player.firstname,
+      player.lastname,
+      player.pseudo,
+      player.password,
+      player.email,
+      player.experience,
+      player.credit,
+      player.membreId,
+      player.profilTheme,
+      player.lvl,
+      player.id,
+    ]);
+
+    if (result.affectedRows === 0) {
+      throw new Error("Aucune ligne affectée, l'update a échoué");
+    }
+    return result;
+  }
+
+  // The D of CRUD - Delete operation
+  // TODO: Implement the delete operation to remove an item by its ID
+
+  async delete(id) {
+    const [rows] = await this.database.query(
+      `DELETE FROM ${this.table} WHERE id=?`,
+      [id]
+    );
+
+    return rows;
+  }
+}
+
+module.exports = PlayerManager;

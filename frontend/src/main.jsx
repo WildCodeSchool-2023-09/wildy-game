35e4a115077fe72fca46948ed5991d36126c6f76
ghispell <ghis.pell@hotmail.fr>
2023-12-19 09:39:35 +0100

    sign up page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -26,6 +26,10 @@ const router = createBrowserRouter([
         path: "login",
         element: <Login />,
       },
+      /*  {
+        path: "signup",
+        element: <Signup />,
+      }, */
     ],
   },
 ]);

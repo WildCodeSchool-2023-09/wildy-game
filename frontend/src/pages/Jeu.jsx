3982dfedea477869001034591191912e904d043b
Océane Lahoual <138216585+Midunighto@users.noreply.github.com>
2024-01-04 17:30:26 +0100

    game page done

--- frontend/src/pages/Jeu.jsx
+++ frontend/src/pages/Jeu.jsx
@@ -1,7 +1,35 @@
 import Pacman from "react-pacman";
+import "../styles/game.scss";
+import { useState } from "react";
+import borne from "../assets/images/borne_game.png";
 
 function Jeu() {
-  return <Pacman />;
+  /* PREVENT DEFAULT SCROLL BY KEY */
+  document.addEventListener("keydown", function (e) {
+    if (
+      e.key === "ArrowUp" ||
+      e.key === "ArrowDown" ||
+      e.key === "ArrowLeft" ||
+      e.key === "ArrowRight"
+    ) {
+      e.preventDefault();
+    }
+  });
+
+  /* LAUNCH */
+  const [start, setStart] = useState(false);
+
+  return (
+    <div className="game-container">
+      <img src={borne} alt="" className="borne-arcade" />
+      <div className="game">{start && <Pacman />}</div>
+      {!start && (
+        <button type="button" onClick={() => setStart(!start)}>
+          PLAY
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default Jeu;

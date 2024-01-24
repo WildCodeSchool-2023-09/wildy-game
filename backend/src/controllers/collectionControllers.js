d6de40be671c48151ed396089393af8ffdb9d4cc
Ijuaa <pierre.colpart@gmail.com>
2024-01-24 14:19:07 +0100

    finished Collection routing

---
+++ backend/src/controllers/collectionControllers.js
@@ -0,0 +1,86 @@
+// Import access to database tables
+const tables = require("../tables");
+
+// The B of BREAD - Browse (Read All) operation
+const browse = async (req, res, next) => {
+  try {
+    // Fetch all items from the database
+    const collection = await tables.collection.readAll();
+
+    res.json(collection);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// The R of BREAD - Read operation
+const findById = async (req, res, next) => {
+  try {
+    // Fetch a specific item from the database based on the provided ID
+    const collection = await tables.collection.read(req.params.id);
+
+    // If the item is not found, respond with HTTP 404 (Not Found)
+    // Otherwise, respond with the item in JSON format
+    if (!collection) {
+      res.sendStatus(404);
+    } else {
+      res.json(collection);
+    }
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
+// The E of BREAD - Edit (Update) operation
+const edit = async (req, res, next) => {
+  try {
+    const collectionToUpdate = {
+      id: parseInt(req.params.id, 10),
+      ...req.body,
+    };
+    const result = await tables.collection.update(collectionToUpdate);
+    if (result.affectedRows === 0) {
+      return res.status(404).send("Collection not found with this ID");
+    }
+    return res.status(200).send(`Collection ${collectionToUpdate.id} updated`);
+  } catch (err) {
+    next(err);
+  }
+  return res.status(200).send("Edit operation completed");
+};
+// This operation is not yet implemented
+
+// The A of BREAD - Add (Create) operation
+const add = async (req, res, next) => {
+  const newCollection = req.body;
+  try {
+    const insertId = await tables.collection.create(newCollection);
+    res.status(201).json({ insertId });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// The D of BREAD - Destroy (Delete) operation
+const destroy = async (req, res, next) => {
+  try {
+    const result = await tables.collection.delete(req.params.id);
+    if (result.affectedRows === 0) {
+      return res.status(404).send("Collection not found with this ID");
+    }
+    return res.status(200).send(`Collection ${req.params.id} deleted`);
+  } catch (err) {
+    next(err);
+  }
+  return res.status(200).send("Destroy operation completed");
+};
+
+// Ready to export the controller functions
+module.exports = {
+  browse,
+  findById,
+  add,
+  edit,
+  destroy,
+};
---
+++ backend/src/models/collectionManager.js
@@ -0,0 +1,71 @@
+const AbstractManager = require("./AbstractManager");
+
+class collectionManager extends AbstractManager {
+  constructor() {
+    // Call the constructor of the parent class (AbstractManager)
+    // and pass the table name "item" as configuration
+    super({ table: "collection" });
+  }
+
+  // The C of CRUD - Create operation
+
+  async create(collection) {
+    const [result] = await this.database.query(
+      `insert into ${this.table} (playerId, avatarId) values (?, ?)`,
+      [collection.playerId, collection.avatarId]
+    );
+    return result.insertId;
+  }
+
+  // The Rs of CRUD - Read operations
+
+  async read(id) {
+    // Execute the SQL SELECT query to retrieve a specific item by its ID
+    const [rows] = await this.database.query(
+      `select * from ${this.table} where id = ?`,
+      [id]
+    );
+
+    // Return the first row of the result, which represents the item
+    return rows[0];
+  }
+
+  async readAll() {
+    // Execute the SQL SELECT query to retrieve all items from the "item" table
+    const [rows] = await this.database.query(`select * from ${this.table}`);
+
+    // Return the array of items
+    return rows;
+  }
+
+  // The U of CRUD - Update operation
+  // TODO: Implement the update operation to modify an existing item
+
+  async update(collection) {
+    if (!collection.id) {
+      throw new Error("Missing ID");
+    }
+
+    const query = `update ${this.table} set playerId = ?, avatarId = ? where id = ?`;
+    const [result] = await this.database.query(query, [
+      collection.playerId,
+      collection.avatarId,
+      collection.id,
+    ]);
+
+    if (result.affectedRows === 0) {
+      throw new Error("Item not found");
+    }
+    return result;
+  }
+
+  async delete(id) {
+    const [rows] = await this.database.query(
+      `delete from ${this.table} where id = ?`,
+      [id]
+    );
+    return rows;
+  }
+}
+
+module.exports = collectionManager;
